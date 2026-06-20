75ef6afea2bd9efa9cec2490c12446a94e1336c9
Acthel12 <160182015+Acthel12@users.noreply.github.com>
2026-06-19 23:05:36 -0400

    feat(home): actualizar navegación con etiquetas multilingües

--- lib/languages/languages.dart
+++ lib/languages/languages.dart
@@ -1,3 +1,10 @@
 class Languages {
   static int selectedLanguageIndex = 0;
+
+  static String about() {
+    return ['Acerca de mi', 'About me'][selectedLanguageIndex];
+  }
+  static String comments() {
+    return ['Comentarios', 'Comments'][selectedLanguageIndex];
+  }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:portafolio_gabriel/languages/languages.dart';
 
 class HomeScreen extends StatefulWidget {
-   
   const HomeScreen({super.key});
 
   @override
@@ -14,22 +14,41 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-      ),
-      body: Center(
-         child: Text('A'),
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.red),
+      body: Stack(
+        children: [
+          PageView(
+            children: [
+              Text('A'),
+              Text('B ')
+            ],
+          )
+        ],
       ),
       bottomNavigationBar: NavigationBar(
         selectedIndex: currentPageIndex,
-        onDestinationSelected: (int index) => setState(() => currentPageIndex = index),
+        onDestinationSelected: (int index) =>
+            setState(() => currentPageIndex = index),
         destinations: [
-          NavigationDestination(icon: FaIcon(FontAwesomeIcons.person), label:  'A'),
-          NavigationDestination(icon: FaIcon(FontAwesomeIcons.person), label:  'A'),
+          NavigationDestination(
+            icon: FaIcon(FontAwesomeIcons.circleUser),
+            label: Languages.about(),
+          ),
+          NavigationDestination(
+            icon: FaIcon(FontAwesomeIcons.heart),
+            label: 'Hobbies',
+          ),
+          NavigationDestination(
+            icon: FaIcon(FontAwesomeIcons.commentDots),
+            label: Languages.comments(),
+          ),
+          NavigationDestination(
+            icon: FaIcon(FontAwesomeIcons.gear),
+            label: 'Hobbies',
+          ),
         ],
       ),
     );
   }
 }
-
