590681a09de7a5cc6d0ed7278ce87e1672d88c81
Acthel12 <160182015+Acthel12@users.noreply.github.com>
2026-06-25 13:09:45 -0400

    fix(main): corregir nombres de clase para consistencia en MainApp

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,14 @@ class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
-  State<MainApp> createState() => _MainAppState();
+  State<MainApp> createState() => MainAppState();
 
-  static _MainAppState? of(BuildContext context) =>
-    context.findAncestorStateOfType<_MainAppState>();
+  static MainAppState? of(BuildContext context) =>
+    context.findAncestorStateOfType<MainAppState>();
   
 }
 
-class _MainAppState extends State<MainApp> {
+class MainAppState extends State<MainApp> {
   ThemeMode themeMode = ThemeMode.system;
 
   void changeTheme(ThemeMode mode) {
